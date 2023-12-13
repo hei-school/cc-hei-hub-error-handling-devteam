4ed574c1a63e462536b4049b8cbb5ce7e911843b
Fanjasoa Andriamanjaka <hei.fanjasoa@gmail.com>
2023-12-13 14:21:14 +0300

    fix: randomNumber

--- randomNumber.py
+++ randomNumber.py
@@ -1,12 +1,17 @@
 import random
-from exceptions import TooManyRequests
+from exceptions import TooManyRequests, RequestTimeout, ServerDown
 
 
 class RandomNumberGenerator:
     @staticmethod
     def generate_and_check():
         random_number = random.randint(1, 100)
-        print(f"Le nombre généré est : {random_number}")
 
         if random_number == 50:
             raise TooManyRequests()
+
+        if random_number == 99:
+            raise RequestTimeout()
+
+        if random_number == 88:
+            raise ServerDown()
